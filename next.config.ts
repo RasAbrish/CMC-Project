1ee2aab813e454a61cb9cefb7ca1d1bc7024532b
RasAbrish <abrhambest7@gmail.com>
2026-02-26 23:34:37 +0300

    feat: Configure Next.js to externalize database packages and add a Prisma seed script.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  serverExternalPackages: ['pg', '@prisma/adapter-pg', '@prisma/adapter-neon', '@neondatabase/serverless']
 };
 
 export default nextConfig;
